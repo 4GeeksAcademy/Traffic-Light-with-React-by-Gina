54684d4de038ba5e4cfe03ce583e545da81cf4d2
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-05-09 11:26:18 +0000

    subo ultimas actualizaciones semáforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
-
-//include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-
-//create your first component
 const Home = () => {
-	const [selectedColor, setSelectedColor] = useState("red");
-	return (
-		<div className="traffic-light mx-2 py-2">
-			<div className={"light red"  + ((selectedColor === "red") ? "glow" : "")}></div>
-			<div className="light yellow"></div>
-			<div className="light green"></div>
-
-
+    const [selectedColor, setSelectedColor] = useState("");
+    return (
+		<div className="container col-2 py-5">
+			<div className="pole"></div>
+				<div className="traffic-light">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={"light red" + (selectedColor === "red" ? " glow" : "")}
+				></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}
+				></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={"light green" + (selectedColor === "green" ? " glow" : "")}
+				></div>
+			</div>
 		</div>
-	);
+    );
 };
 
 export default Home;
