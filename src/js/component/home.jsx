c8b76cf2c9641113b79d1fec8d697a74e9324a1f
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-05-10 16:33:36 +0000

    subo ultimos actualizados semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,26 +2,28 @@ import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 const Home = () => {
-    const [selectedColor, setSelectedColor] = useState("");
-    return (
-		<div className="container col-2 py-5">
-			<div className="pole"></div>
-				<div className="traffic-light">
-				<div
-					onClick={() => setSelectedColor("red")}
-					className={"light red" + (selectedColor === "red" ? " glow" : "")}
-				></div>
-				<div
-					onClick={() => setSelectedColor("yellow")}
-					className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}
-				></div>
-				<div
-					onClick={() => setSelectedColor("green")}
-					className={"light green" + (selectedColor === "green" ? " glow" : "")}
-				></div>
-			</div>
+	const [selectedColor, setSelectedColor] = useState("");
+	return (
+	  <div className="container col-2 ">
+		<div className="pole"></div>
+		<div className="traffic-light py-3 rounded">
+		<div
+          onClick={() => setSelectedColor("red")}
+          className={"light red p-2 " + (selectedColor === "red" ? " glow" : "")}
+        >
+          {selectedColor === "red" && <span className="stop-text">STOP</span>}
+        </div>
+		  <div
+			onClick={() => setSelectedColor("yellow")}
+			className={"light yellow " + (selectedColor === "yellow" ? " glow" : "")}
+		  ></div>
+		  <div
+			onClick={() => setSelectedColor("green")}
+			className={"light green p-2" + (selectedColor === "green" ? " glow" : "")}
+		  ></div>
 		</div>
-    );
-};
-
-export default Home;
+	  </div>
+	);
+  };
+  
+  export default Home;
